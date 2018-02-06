2ca323c87841a00b131964ac2bf830f46af87cc6
Alan Fleming <alalfleming@gmail.com>
2018-02-05 23:56:41 -0500

    functioning buttons

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const app = require('express').Router();
+const db = require('../db');
+const { models } = db;
+const { User } = models;
+
+module.exports = app;
+
+app.get('/', (req, res, next)=>{
+  User.findAll()
+  .then( users => res.render('users', { users }))
+  .catch( err => next(err));
+});
+
+app.post('/', (req, res, next)=>{
+  User.create(req.body)
+  .then( user => res.redirect('/users'))
+  .catch( err => next(err));
+})
+
+app.delete('/:id', (req, res, next)=>{
+  User.findById(req.params.id)
+  .then( user => user.destroy())
+  .then( ()=> res.redirect('/users'))
+  .catch( err => next(err));
+});
