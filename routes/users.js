89f6ca349e1743bdc8e51b3fe1209ccf42d1e8d5
Alan Fleming <alalfleming@gmail.com>
2018-02-06 17:48:05 -0500

    layout set up

--- routes/users.js
+++ routes/users.js
@@ -11,15 +11,26 @@ app.get('/', (req,res,next)=>{
 
 app.get('/users', (req, res, next)=>{
   User.findAll()
-  .then( users => res.render('users', { users }))
+  .then( users => res.render('users', { title: 'Users',users }))
   .catch( err => next(err));
 });
 
-app.post('/users', (req, res, next)=>{
-  User.create(req.body)
-  .then( user => res.redirect('/users'))
-  .catch( err => next(err));
-})
+// app.post('/users', (req, res, next)=>{
+//   User.create(req.body)
+//   .then( user => res.redirect('/users'))
+//   .catch( err => next(err));
+// })
+
+app.post('/users', (req, res, next)=> {
+   if (req.body.name.length === 0) {
+       res.render('error', { title: 'Error' });
+   }
+   else {
+       User.create(req.body)
+       .then( user => res.redirect('/users'))
+       .catch( err => next(err));
+   }
+});
 
 app.delete('/users/:id', (req, res, next)=>{
   User.findById(req.params.id)
@@ -27,3 +38,4 @@ app.delete('/users/:id', (req, res, next)=>{
   .then( ()=> res.redirect('/users'))
   .catch( err => next(err));
 });
+
